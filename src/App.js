e70efbfd02c6440b8733f04fb7c7b8900e746a28
CodeSerch <sergioenriquebg28@gmail.com>
2021-10-06 17:07:15 -0300

    commint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/nav-bar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nueva aplicacion de mi tienda de Postres!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
-        <h1>
-          hola xd
-        </h1>
-      </body>
+      <NavBar/>
     </div>
   );
 }
